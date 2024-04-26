dba95c3b44a99d295e6dc49d2dd9cbf586db128e
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-04-27 00:27:54 +0200

    ladder mechanic

--- scenes/ladder/Ladder.gd
+++ scenes/ladder/Ladder.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+@onready var area_2d = $Area2D
+@onready var start = $Start
+@onready var end = $End
 
-
-func _on_area_2d_body_entered(body):
-	if body == get_tree().get_first_node_in_group("player"):
-		body.current_moon = null
+func _physics_process(delta):
+	if area_2d.overlaps_body(get_tree().get_first_node_in_group("player")):
+		var player = get_tree().get_first_node_in_group("player")
+		if Input.is_action_pressed("up"):
+			var direction_to_end = global_position.direction_to(end.global_position)
+			player.set_axis_velocity(direction_to_end*100)
+		elif Input.is_action_pressed("down"):
+			var direction_to_start = global_position.direction_to(start.global_position)
+			player.set_axis_velocity(direction_to_start*100)
