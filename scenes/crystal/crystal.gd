5e0500fa8cec03eff5ca08544c63f667a8d0029d
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-04-26 22:50:25 +0200

    scene

---
+++ scenes/crystal/crystal.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var area_2d = $Area2D
+
+func _process(delta):
+	if area_2d.overlaps_body(get_tree().get_first_node_in_group("player")):
+		if Input.is_action_just_pressed("interact"):
+			print("Crystal")
