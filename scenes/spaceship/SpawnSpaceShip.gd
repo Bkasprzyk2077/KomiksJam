60675659895cf9d412f78f600cff857aadbada4d
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-04-27 13:44:16 +0200

    space ship path

--- scenes/spaceship/SpawnSpaceShip.gd
+++ scenes/spaceship/SpawnSpaceShip.gd
@@ -1,27 +1,15 @@
-extends Node2D
+extends PathFollow2D
 
 var points_to_visit = []
 var next_point
 
 @export var speed: float = 200
+@onready var path_follow_2d = $Path2D/PathFollow2D
 
-func _ready():
-	points_to_visit = $Points.get_children()
-	print(points_to_visit)
-	next_point = get_next_point()
-	
 func _process(delta):
-	move_to_point(delta)
-	
-func move_to_point(delta):
-	if global_position == next_point:
-		next_point = get_next_point()
+	print(progress_ratio)
+	if progress_ratio < 0.95: 
+		progress += delta * speed
 	else:
-		global_position = global_position.move_toward(next_point, delta * speed)
-
-func get_next_point():
-	var point = points_to_visit.pop_front()
-	if point == null:
 		queue_free()
 		return
-	return point.global_position
