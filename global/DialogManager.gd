98af3538b67f4271c70d99b4a609fcedf7b99ce3
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-04-27 02:30:51 +0200

    dialog logic

---
+++ global/DialogManager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@onready var dialog_panel_scene = preload("res://ui/dialog_panel.tscn")
+var dialog_canvas
+
+func start_dialog(dialog_text_file):
+	dialog_canvas = get_tree().get_first_node_in_group("dialog_canvas")
+	var dialog_panel_instance = dialog_panel_scene.instantiate()
+	dialog_canvas.add_child(dialog_panel_instance)
+	await dialog_panel_instance.get_node("AnimationPlayer").animation_finished
+	dialog_panel_instance.set_up_text(dialog_text_file)
+	
+
