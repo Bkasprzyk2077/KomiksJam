148150548837f40dcf2584ff9680c85f2fc6cce5
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-04-27 09:31:34 +0200

    enemy kick

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,10 +1,11 @@
 extends RigidBody2D
 
 @onready var death_area = $DeathArea
-
+@export var speed = 200
 
 var current_moon
 var moon_direction
+var move_direction = 1
 
 func _physics_process(delta):
 	if death_area.overlaps_body(get_tree().get_first_node_in_group("player")):
@@ -15,11 +16,20 @@ func _physics_process(delta):
 	else:
 		moon_direction = Vector2.ZERO
 		
-	var move_direction = 1
-	set_axis_velocity(moon_direction.rotated(deg_to_rad(85 * -move_direction)) * 300)
+	set_axis_velocity(moon_direction.rotated(deg_to_rad(85 * -move_direction)) * speed)
 
 
 func death():
-	var player = get_tree().get_first_node_in_group("player")
-	player.apply_impulse(-moon_direction * 1000)
+	#var player = get_tree().get_first_node_in_group("player")
+	#player.apply_impulse(-moon_direction * 1000)
 	queue_free()
+
+
+
+
+func _on_player_kicker_body_entered(body):
+	move_direction *= -1
+	$PlayerKicker.scale.x *= -1
+	var player = get_tree().get_first_node_in_group("player")
+	if body == player:
+		player.apply_impulse(global_position.direction_to(player.global_position) * 1000)
